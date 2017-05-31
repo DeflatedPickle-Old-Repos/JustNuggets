d0f05903120814892ad6947e80e6ab135eb6101f
Dibbo <DeflatedPickle@gmail.com>
2017-05-31 18:19:48 +0100

    Registers a colour for each nugget

--- src/main/java/com/deflatedpickle/justnuggets/JustNuggets.java
+++ src/main/java/com/deflatedpickle/justnuggets/JustNuggets.java
@@ -4,9 +4,15 @@ import com.deflatedpickle.justnuggets.init.ModCrafting;
 import com.deflatedpickle.justnuggets.items.ItemNugget;
 import com.deflatedpickle.justnuggets.proxy.CommonProxy;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.renderer.color.IItemColor;
+import net.minecraft.client.renderer.color.ItemColors;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
@@ -15,7 +21,12 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.oredict.OreDictionary;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION, acceptedMinecraftVersions = Reference.ACCEPTED_VERSIONS, dependencies = "after:*")
 public class JustNuggets {
@@ -27,8 +38,11 @@ public class JustNuggets {
 
     public static ItemNugget nugget;
 
+    public static List<Item> nugget_list = new ArrayList();
+
     @EventHandler
     public void preInit(FMLPreInitializationEvent event){
+        ItemColors itemColors = Minecraft.getMinecraft().getItemColors();
         for (String block : OreDictionary.getOreNames())
             // Search for all blocks in the OreDictionary
             if (block.startsWith("block") && !OreDictionary.getOres(block).isEmpty()){
@@ -40,6 +54,10 @@ public class JustNuggets {
                         nugget = new ItemNugget(metal);
                         // Register the nugget
                         GameRegistry.register(nugget);
+                        // Register rendering of the nugget
+                        ModelLoader.setCustomModelResourceLocation(nugget, 0, new ModelResourceLocation("justnuggets" + ":" + "nugget", "inventory"));
+                        // Adds the nugget to the list
+                        nugget_list.add(nugget);
                         // Add crafting recipe
                         ModCrafting.add_recipe(nugget, metal);
                         // Register to ore dictionary
@@ -52,10 +70,21 @@ public class JustNuggets {
     public void Init(FMLInitializationEvent event){
         proxy.init();
         ModCrafting.register();
+        // Runs through each nugget
+        for (Object nug : nugget_list) {
+            // Registers the colour for each nugget
+            registerItemColor((Item) nug, (IItemColor) nug);
+        }
     }
 
     @EventHandler
     public void PostInit(FMLPostInitializationEvent event){
+    }
+
+    @SideOnly(Side.CLIENT)
+    public static void registerItemColor(Item nugget, IItemColor color){
+        ItemColors itemColors = Minecraft.getMinecraft().getItemColors();
+        itemColors.registerItemColorHandler(color::getColorFromItemstack, nugget);
     }
 
     public static CreativeTabs tabJustNuggets = new CreativeTabs("tab_justnuggets"){
--- src/main/java/com/deflatedpickle/justnuggets/items/ItemNugget.java
+++ src/main/java/com/deflatedpickle/justnuggets/items/ItemNugget.java
@@ -2,12 +2,21 @@ package com.deflatedpickle.justnuggets.items;
 
 import com.deflatedpickle.justnuggets.JustNuggets;
 import com.deflatedpickle.justnuggets.Reference;
+import net.minecraft.client.renderer.color.IItemColor;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class ItemNugget extends Item{
+public class ItemNugget extends Item implements IItemColor{
     public ItemNugget(String base){
         setUnlocalizedName("nugget" + base.substring("ingot".length()));
         setRegistryName(Reference.MOD_ID + ":" + base.substring("ingot".length()) + "_nugget");
         setCreativeTab(JustNuggets.tabJustNuggets);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public int getColorFromItemstack(ItemStack stack, int tint){
+        return 0xff99ff;
     }
 }
