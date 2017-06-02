1b89131a42e9c991e15b72e289bd3771ce6aa337
Dibbo <DeflatedPickle@gmail.com>
2017-06-02 05:20:41 +0100

    Nuggets now use the colour of the ingot

--- src/main/java/com/deflatedpickle/justnuggets/JustNuggets.java
+++ src/main/java/com/deflatedpickle/justnuggets/JustNuggets.java
@@ -5,9 +5,15 @@ import com.deflatedpickle.justnuggets.items.ItemNugget;
 import com.deflatedpickle.justnuggets.proxy.CommonProxy;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.renderer.color.IItemColor;
 import net.minecraft.client.renderer.color.ItemColors;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.IReloadableResourceManager;
+import net.minecraft.client.resources.IResourceManager;
+import net.minecraft.client.resources.IResourceManagerReloadListener;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
@@ -38,7 +44,7 @@ public class JustNuggets {
 
     public static ItemNugget nugget;
 
-    public static List<Item> nugget_list = new ArrayList();
+    public static List<ItemNugget> nugget_list = new ArrayList();
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event){
@@ -63,11 +69,20 @@ public class JustNuggets {
     public void Init(FMLInitializationEvent event){
         proxy.init();
         ModCrafting.register();
-        // Runs through each nugget
-        for (Object nug : nugget_list) {
-            // Registers the colour for each nugget
-            registerItemColor((Item) nug, (IItemColor) nug);
-        }
+        ((IReloadableResourceManager) Minecraft.getMinecraft().getResourceManager()).registerReloadListener(new IResourceManagerReloadListener() {
+            @Override
+            public void onResourceManagerReload(IResourceManager resourceManager) {
+                for (ItemNugget nug : nugget_list) {
+                    for (ItemStack ingot : OreDictionary.getOres(String.format("ingot%s", nug.base.substring(5)))) {
+                        registerNuggetColour(ingot, nug);
+                    }
+                    for (ItemStack gem : OreDictionary.getOres(String.format("gem%s", nug.base.substring(3)))) {
+                        registerNuggetColour(gem, nug);
+                    }
+                    registerItemColor(nug, nug);
+                }
+            }
+        });
     }
 
     @EventHandler
@@ -100,5 +115,17 @@ public class JustNuggets {
         ModCrafting.add_recipe(nugget, base);
         // Register to ore dictionary
         OreDictionary.registerOre("nugget" + base, nugget);
+    }
+
+    private void registerNuggetColour(ItemStack base, ItemNugget nug){
+        IBakedModel model = Minecraft.getMinecraft().getRenderItem().getItemModelWithOverrides(base, null, null);
+        List<BakedQuad> quads = model.getQuads(null, null, 0);
+        for (BakedQuad quad : quads) {
+            TextureAtlasSprite sprite = quad.getSprite();
+            int[] rgba = sprite.getFrameTextureData(0)[0];
+            int colour = rgba[7 + 7 * sprite.getIconWidth()];
+            // System.out.println(String.format("Nugget: %s | Ingot: %s | Texture Name: %s | RGBA: %s", nug, base, sprite.getIconName(), colour));
+            nug.colour = colour;
+        }
     }
 }
--- src/main/java/com/deflatedpickle/justnuggets/items/ItemNugget.java
+++ src/main/java/com/deflatedpickle/justnuggets/items/ItemNugget.java
@@ -9,14 +9,28 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ItemNugget extends Item implements IItemColor{
+    // TODO: Implement IItemColor somewhere else, I've been told it's bad to do it here.
+    // TODO: Use the texture for the golden nugget and greyscale it automatically.
+    public String base;
+    public String type;
+    public int colour;
+
     public ItemNugget(String base, String type){
+        this.base = base;
+        this.type = type;
+
         setUnlocalizedName("nugget" + base.substring(type.length()));
         setRegistryName(Reference.MOD_ID + ":" + base.substring(type.length()) + "_nugget");
         setCreativeTab(JustNuggets.tabJustNuggets);
     }
 
+    @Override
+    public String getItemStackDisplayName(ItemStack stack){
+        return String.format("%s Nugget", this.base.substring(type.length()));
+    }
+
     @SideOnly(Side.CLIENT)
     public int getColorFromItemstack(ItemStack stack, int tint){
-        return 0xff99ff;
+        return this.colour;
     }
 }
